773aef28f66487d24abff9050bd2c19664678950
ddelarosae <ddelarosae@uninorte.edu.co>
2022-09-10 07:19:58 -0500

    eliminacion de tiburon de la lista miVertebrados

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,10 +1,9 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
-        Vertebrado[] miVertebrados = new Vertebrado[4];
+        Vertebrado[] miVertebrados = new Vertebrado[3];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Perro", "Mamifero");
-        miVertebrados[3] = new Vertebrado("Tiburón", "Pez");         
+        miVertebrados[2] = new Vertebrado("Perro", "Mamifero");      
         return miVertebrados;
     }
 }
